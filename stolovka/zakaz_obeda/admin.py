6a92d5f5102f552a34e4ba173f131b8ccafcb61d
Stepan <stepan.kalakin@gmail.com>
2026-01-28 15:51:55 +0300

    add models

--- stolovka/zakaz_obeda/admin.py
+++ stolovka/zakaz_obeda/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Dish
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка для категории"""
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    """Админка для блюд"""
+    list_display = ('name', 'price', 'about', 'category')
+    search_fields = ('name',)
+    list_per_page = 20
+    list_filter = ('category',)
--- stolovka/zakaz_obeda/models.py
+++ stolovka/zakaz_obeda/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название категории')
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название блюда')
+    price = models.FloatField(null=True, blank=True, verbose_name='Стоимость')
+    about = models.TextField(null=True, blank=True, verbose_name='Описание')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        verbose_name='Категория'
+    )
+    
+    class Meta:
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
