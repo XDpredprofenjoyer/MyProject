901f85b85ec7130b1c9e984cc35bd96d103fab97
Stepan <stepan.kalakin@gmail.com>
2026-02-02 15:55:59 +0300

    add new table and fields

--- stolovka/zakaz_obeda/admin.py
+++ stolovka/zakaz_obeda/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Dish
+from .models import Category, Dish, Ingridients, Menu, Application, Feedback, Feedback_category, СomplexDish
 
 
 @admin.register(Category)
@@ -11,11 +11,63 @@ class CategoryAdmin(admin.ModelAdmin):
     list_per_page = 20
 
 
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    """Админка для меню"""
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 20
+    filter_horizontal = ("complex_dishes", "dishes")
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    """Админка для заявки"""
+    list_display = ('name', 'date', 'amount', 'select_dishes',)
+    search_fields = ('name',)
+    list_per_page = 20
+
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     """Админка для блюд"""
-    list_display = ('name', 'price', 'about', 'category')
+    list_display = ('name', 'price', 'about', 'in_menu', 'category')
     search_fields = ('name',)
     list_per_page = 20
-    list_filter = ('category',)
+    list_filter = ('category', "ingridients")
+    filter_horizontal = ("ingridients",)
+
+
+@admin.register(СomplexDish)
+class СomplexDishAdmin(admin.ModelAdmin):
+    """Админка для комплексных обедов или завтраков"""
+    list_display = ('name', 'price', 'about', 'in_menu')
+    search_fields = ('name',)
+    list_per_page = 20
+    list_filter = ('dishes',)
+    filter_horizontal = ("dishes",)
+
+
+@admin.register(Ingridients)
+class IngridientsAdmin(admin.ModelAdmin):
+    """Админка для ингридиентов"""
+    list_display = ('name', 'is_allergen')
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    """Админка для отзывов"""
+    list_display = ('name', 'sender', 'about', 'is_good', 'feedback_category')
+    search_fields = ('name',)
+    list_per_page = 20
+    list_filter = ('feedback_category',)
+
+
+@admin.register(Feedback_category)
+class Feedback_categoryAdmin(admin.ModelAdmin):
+    """Админка для категорий отзывов"""
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_per_page = 20
--- stolovka/zakaz_obeda/models.py
+++ stolovka/zakaz_obeda/models.py
@@ -11,10 +11,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Ingridients(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название ингридиента')
+    is_allergen = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Название блюда')
     price = models.FloatField(null=True, blank=True, verbose_name='Стоимость')
     about = models.TextField(null=True, blank=True, verbose_name='Описание')
+    in_menu = models.BooleanField(default=False, verbose_name='В архиве')
     category = models.ForeignKey(
         Category, 
         on_delete=models.SET_NULL, 
@@ -22,10 +37,124 @@ class Dish(models.Model):
         blank=True, 
         verbose_name='Категория'
     )
+    ingridients = models.ManyToManyField(
+        Ingridients, 
+        blank=True, 
+        verbose_name='Ингридиенты'
+    )
     
     class Meta:
         verbose_name = 'Блюдо'
         verbose_name_plural = 'Блюда'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+class СomplexDish(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название комплекса')
+    price = models.FloatField(null=True, blank=True, verbose_name='Стоимость')
+    about = models.TextField(null=True, blank=True, verbose_name='Описание')
+    in_menu = models.BooleanField(default=False, verbose_name='В архиве' )
+    dishes = models.ManyToManyField(
+        Dish, 
+        blank=True, 
+        verbose_name='Блюда в комплексе'
+    )
+    
+    class Meta:
+        verbose_name = 'Комплекс'
+        verbose_name_plural = 'Комплексы'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+class Application(models.Model):
+    
+    STATUS_CHOICES = (
+        ("NEW", "Новый"),
+        ("DONE", "Оплата проведена"),
+    )
+
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название заявки')
+    date = models.DateField(null=True, blank=True, verbose_name='Дата')
+    amount = models.FloatField(null=True, blank=True, verbose_name='Стоимость')
+    select_dishes = models.ForeignKey(
+    Dish, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        verbose_name='Выбор блюд'
+    )
+    status = models.CharField(max_length=9,
+                    choices=STATUS_CHOICES,
+                 verbose_name='Статус',
+                  default="NEW")
+    
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Feedback_category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название категории')
+    
+    class Meta:
+        verbose_name = 'Категория отзыва'
+        verbose_name_plural = 'Категории отзыва'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название отзыва')
+    sender = models.CharField(max_length=100, unique=True, verbose_name='Имя отправителя')
+    about = models.TextField(null=True, blank=True, verbose_name='Описание')
+    is_good = models.FloatField(null=True, blank=True, verbose_name='Оценка')
+    feedback_category = models.ForeignKey(
+        Feedback_category,
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        verbose_name='Категория отзыва'
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название меню')
+    complex_dishes = models.ManyToManyField(
+        СomplexDish, 
+        blank=True, 
+        verbose_name='Комплекс в меню'
+    )
+    dishes = models.ManyToManyField(
+        Dish, 
+        blank=True, 
+        verbose_name='Блюда в меню'
+    )
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
         ordering = ['name']
     
     def __str__(self):
